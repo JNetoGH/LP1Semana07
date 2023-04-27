4a4da7438d87e734e5ba5da5100d4a19815d1e07
JNeto <joaomendes15@gmail.com>
2023-04-28 00:48:47 +0100

    Get Grey added and Sphere class added

--- FillSpheres/Color.cs
+++ FillSpheres/Color.cs
@@ -13,14 +13,16 @@
             _green = green;
         }
 
-        public byte GetRed() => this._red;
-        public void SetRed(byte newValue) => this._red = newValue;
+        public byte GetRed() => _red;
+        public void SetRed(byte newValue) => _red = newValue;
 
-        public byte GetBlue() => this._blue;
-        public void SetBlue(byte newValue) => this._blue = newValue;
+        public byte GetBlue() => _blue;
+        public void SetBlue(byte newValue) => _blue = newValue;
         
-        public byte GetGreen() => this._green;           
-        public void SetGreen(byte newValue) => this._green = newValue;
+        public byte GetGreen() => _green;           
+        public void SetGreen(byte newValue) => _green = newValue;
+
+        public byte GetGrey() => (byte) ((_red + _blue + _red) / 3);
 
     }
 }
---
+++ FillSpheres/Sphere.cs
@@ -0,0 +1,24 @@
+﻿namespace FillSpheres
+{
+    public class Sphere
+    {
+        private Color _color;
+        private float _radius;
+        private uint _timesThrown;
+
+        public Sphere(Color color, float radius)
+        {
+            _color = color;
+            _radius = radius;
+            _timesThrown = 0;
+        }
+
+        /// <summary> Pops the ball by setting its radius to 0 </summary>
+        public void Pop() => _radius = 0;
+        
+        /// <summary> Throws the Sphere in case it hasn't been popped </summary>
+        public void Throw() => _timesThrown = _timesThrown > 0 ? _timesThrown + 1 : _timesThrown;
+        
+        public uint GetTimesThrown() => _timesThrown;
+    }
+}
