3b7046dcc0c793790a5f3f7b1d820fc98a401f81
JNeto <joaomendes15@gmail.com>
2023-04-28 00:20:31 +0100

    Color fields added

---
+++ FillSpheres/Color.cs
@@ -0,0 +1,17 @@
+﻿namespace FillSpheres
+{
+    public class Color
+    {
+        private byte _red = 0;
+        private byte _blue = 0;
+        private byte _green = 0;
+                            
+        public Color(byte red, byte blue, byte green)
+        {
+            _red = red;
+            _blue = blue;
+            _green = green;
+        }
+
+    }
+}
