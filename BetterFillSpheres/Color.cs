e3e619ae72f1c83cededb9f1ed0621d8f360b99f
JNeto <joaomendes15@gmail.com>
2023-04-28 01:29:48 +0100

    GetGrey changed to read-only property

---
+++ BetterFillSpheres/Color.cs
@@ -0,0 +1,21 @@
+﻿namespace BetterFillSpheres
+{
+    public class Color
+    {
+        private byte Red { get; set; } = 0;
+        private byte Blue { get; set; } = 0;
+        private byte Green { get; set; } = 0;
+        private byte Alpha { get; set; } = 0;
+        private byte Grey => (byte)((Red + Blue + Red) / 3);
+                            
+        public Color(byte red, byte blue, byte green, byte alpha)
+        {
+            Red = red;
+            Blue = blue;
+            Green = green;
+            Alpha = alpha;
+        }
+        
+        public override string ToString() => $"r:{Red}, b:{Blue}, g:{Green}, a:{Alpha}";
+    }
+}
