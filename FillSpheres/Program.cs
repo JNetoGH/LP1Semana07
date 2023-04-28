adcd5de386f6a660e97f46f4a370eea2ef12ebe9
JNeto <joaomendes15@gmail.com>
2023-04-28 01:16:29 +0100

    Main implemented as required

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -6,8 +6,31 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            var s1 = new Sphere(new Color(255, 255, 255, 255), 5);
-            Console.WriteLine(s1.GetColor());
+
+            Sphere[] spheres = new Sphere[3]
+            {
+                new Sphere(new Color(255, 0, 0, 255), 1.0f),
+                new Sphere(new Color(0, 255, 0, 255), 5.0f),
+                new Sphere(new Color(0, 0, 255, 255), 2.5f)
+            };
+
+            
+            Console.WriteLine("\nSPHERES:");
+            foreach (Sphere sphere in spheres)
+                Console.WriteLine(sphere);
+
+            // play around with spheres
+            spheres[0].Throw();
+            spheres[1].Throw();
+            spheres[1].Pop();
+            spheres[2].Throw();
+            spheres[2].Throw();
+
+            Console.WriteLine("\nplay around with spheres\n");
+
+            Console.WriteLine("SPHERES:");
+            foreach (Sphere sphere in spheres)
+                Console.WriteLine(sphere);
             
         }
     }
--- FillSpheres/Sphere.cs
+++ FillSpheres/Sphere.cs
@@ -17,9 +17,10 @@
         public void Pop() => _radius = 0;
         
         /// <summary> Throws the Sphere in case it hasn't been popped </summary>
-        public void Throw() => _timesThrown = _timesThrown > 0 ? _timesThrown + 1 : _timesThrown;
+        public void Throw() => _timesThrown = _radius > 0 ? _timesThrown + 1 : _timesThrown;
         
         public Color GetColor() => _color;
         public uint GetTimesThrown() => _timesThrown;
+        public override string ToString() => $"color:({_color}), radius:{_radius}, times thrown:{_timesThrown}";
     }
 }
